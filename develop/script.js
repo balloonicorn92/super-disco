8c286bcb04cc3fb294082839c08ef29ff0538b65
Fernanda Frers <fernanda.frers@gmail.com>
2021-03-10 13:51:14 -0600

    changed id names to numbers

--- develop/script.js
+++ develop/script.js
@@ -10,40 +10,32 @@ $(document).ready(function(){
         localStorage.setItem(timeKey, userInput)
     })
     //identifier get from local storage
-        $("#nine .description").val(localStorage.getItem("nine"));
-        $("#ten .description").val(localStorage.getItem("ten"));
-        $("#eleven .description").val(localStorage.getItem("eleven"));
-        $("#twelve .description").val(localStorage.getItem("twelve"));
-        $("#thirteen .description").val(localStorage.getItem("thirteen"));
-        $("#fourteen .description").val(localStorage.getItem("fourteen"));
-        $("#fifteen .description").val(localStorage.getItem("fifteen"));
-        $("#sixteen .description").val(localStorage.getItem("sixteen"));
-        $("#seventeen .description").val(localStorage.getItem("seventeen"));
-        $("#eighteen .description").val(localStorage.getItem("eighteen"));
+        $("#9 .description").val(localStorage.getItem("9"));
+        $("#10 .description").val(localStorage.getItem("10"));
+        $("#11 .description").val(localStorage.getItem("11"));
+        $("#12 .description").val(localStorage.getItem("12"));
+        $("#13 .description").val(localStorage.getItem("13"));
+        $("#14 .description").val(localStorage.getItem("14"));
+        $("#15 .description").val(localStorage.getItem("15"));
+        $("#16 .description").val(localStorage.getItem("16"));
+        $("#17 .description").val(localStorage.getItem("17"));
+        $("#18 .description").val(localStorage.getItem("18"));
     //onclick time block textarea edit... on blur save... make sure to call it
     let timeOfDay = function() {
         let currentTime = moment().hours()
 
         $(".time-block").each(function(){
-            let divTime = $(this).attr("id")
-            
+            let divTime = parseInt($(this).attr("id"))
+            if(divTime < currentTime){
+                $(this).addClass("past")
+            } else if(divTime === currentTime){
+                $(this).removeClass("past")
+                $(this).addClass("present")
+            } else if (divTime > currentTime){
+                $(this).removeClass("present past")
+                $(this).addClass("future")
+            }
         })
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    timeOfDay()
 })
